9d44d214462d94b1d926e3a070ed53e9e7ba3507
eunsagn <dmstkdrmatkd@naver.com>
2020-08-16 10:54:57 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+
+
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = 0;
+    
+    return answer;
+}
+
+
+int main() {
+  
+  int leng = 2;
+  int weight = 10;
+  vector<int> truck = {7,4,5,6};
+
+  solution(leng, weight, truck);
+}
